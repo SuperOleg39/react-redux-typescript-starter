842cdce7591c814ea819b602737ff7473594353b
SuperOleg39 <surf4sites@yandex.ru>
2017-10-15 13:07:05 +0300

    Clean step-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,13 @@
-import 'core-js/es6/promise';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { IModuleProps } from './module';
 
 interface IAppProps {
     title: string;
 }
 
-interface IAppState {
-    Module: React.ComponentClass<IModuleProps>;
-}
+const App = (props: IAppProps) => <h1>{props.title}</h1>;
 
-class App extends React.Component<IAppProps, IAppState> {
-    state: IAppState = { Module: null };
-    
-    async componentDidMount() {
-        try {
-            const lazy = await import('./module');
-            const Module = lazy.Module;
-            
-            this.setState({ Module });
-        } catch (e) {
-            console.log('Error Module loading', e);
-        }
-    }
-    
-    render() {
-        console.log('App is render');
-        const { title } = this.props;
-        const { Module } = this.state;
-        
-        return (
-            <div>
-                <h1>{title}</h1>
-                {Module ? <Module description="Lazy loaded Component" /> : <div>...</div>}
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<App title="Application" />, document.getElementById('root'));
+ReactDOM.render(
+    <App title="Hello, React!" />,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const env = process.env.NODE_ENV;
-const __DEV__ = env === 'development';
-const __PRODUCTION__ = env === 'production';
 
 const paths = {
     src: path.resolve(__dirname, 'src'),
@@ -28,6 +23,8 @@ const config = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
+
+    devtool: 'inline-source-map',
     
     module: {
         rules: [
@@ -39,28 +36,11 @@ const config = {
     },
     
     plugins: [
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static'
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(env)
-        }),
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             template: './index.html'
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin()
+        })
     ]
 };
-
-if (__DEV__) {
-    config.devtool = 'inline-source-map';
-    config.plugins.push(new webpack.NamedModulesPlugin());
-}
-
-if (__PRODUCTION__) {
-    config.plugins.push(new CleanWebpackPlugin(['dist']));
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-    config.plugins.push(new webpack.HashedModuleIdsPlugin());
-}
 
 module.exports = config;
